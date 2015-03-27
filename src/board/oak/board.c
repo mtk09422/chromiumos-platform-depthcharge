6c2087bc3cb8b017ca9e44f30d2b6447a8ed4ac6
Chunfeng Yun <chunfeng.yun@mediatek.com>
2015-03-27 18:34:07 +0800

    Oak: initialize USB host
    BRANCH=none
    BUG=none
    TEST=none
    
    Change-Id: I3f219ade187ea5d9fb5bbc5835f30a3ffa3b543a
    Signed-off-by: Chunfeng Yun <chunfeng.yun@mediatek.com>

--- src/board/oak/board.c
+++ src/board/oak/board.c
@@ -79,6 +79,10 @@ static int board_setup(void)
 		display_set_ops(new_mt8173_display());
 	}
 
+	UsbHostController *usb_host = new_usb_hc(XHCI, 0x11270000);
+
+	list_insert_after(&usb_host->list_node, &usb_host_controllers);
+
 	/* Setup sound components */
 	MtkI2s *i2s0 = new_mtk_i2s(0x11220000, 16, 2, 48000,
 				   0x10209000, 0x10000000);
